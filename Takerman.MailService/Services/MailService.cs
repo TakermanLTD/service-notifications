ccf1f6b2c5aea52f9fadc8826aeb2407407fcb0b
Tanyo Ivanov <tivanov@takerman.net>
2023-10-19 22:47:45 +0300

    Simplify

--- Takerman.MailService/Services/MailService.cs
+++ Takerman.MailService/Services/MailService.cs
@@ -1,34 +1,17 @@
 ﻿using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using RabbitMq.Common.Models;
-using RabbitMQ.Client;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Channels;
-using Takerman.MailService.Models;
-using Takerman.MailService.Queue;
 
 namespace Takerman.MailService.Consumer.Services
 {
     public class MailService : IMailService
     {
         private readonly SmtpConfig _smtpConfig;
-        private readonly RabbitMqConfig _rabbitMqConfig;
-        private readonly ConnectionFactory _connectionFactory;
 
-        public MailService(IOptions<SmtpConfig> smtpConfig, IOptions<RabbitMqConfig> rabbitMqConfig)
+        public MailService(IOptions<SmtpConfig> smtpConfig)
         {
             _smtpConfig = smtpConfig.Value;
-            _rabbitMqConfig = rabbitMqConfig.Value;
-            _connectionFactory = new ConnectionFactory()
-            {
-                HostName = _rabbitMqConfig.Hostname,
-                UserName = _rabbitMqConfig.Username,
-                Password = _rabbitMqConfig.Password,
-                Port = _rabbitMqConfig.Port,
-                DispatchConsumersAsync = true
-            };
         }
 
         public async Task Send(MailMessage message)
@@ -51,54 +34,6 @@ namespace Takerman.MailService.Consumer.Services
             catch (Exception ex)
             {
                 throw new Exception("Cannot send email", ex);
-            }
-        }
-
-        public async Task SendToQueue(MailMessage message)
-        {
-            var messageDto = new MailMessageDto()
-            {
-                From = message.From.Address,
-                To = message.To.FirstOrDefault().Address,
-                Body = message.Body,
-                Subject = message.Subject
-            };
-
-            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageDto));
-
-            using (var connection = _connectionFactory.CreateConnection())
-            {
-                using (var channel = connection.CreateModel())
-                {
-                    try
-                    {
-                        channel.ExchangeDeclare(DeadLetterQueue.Exchange, ExchangeType.Direct, durable: true, autoDelete: false);
-                        channel.QueueDeclare(DeadLetterQueue.Queue, durable: true, exclusive: false, autoDelete: false);
-                        channel.QueueBind(DeadLetterQueue.Queue, DeadLetterQueue.Exchange, DeadLetterQueue.RoutingKey);
-
-                        channel.ExchangeDeclare(MailQueue.Exchange, ExchangeType.Direct, durable: false, autoDelete: false);
-                        channel.QueueDeclare(MailQueue.Queue, durable: false, exclusive: false, autoDelete: false, DeadLetterQueue.Args);
-                        channel.QueueBind(MailQueue.Queue, MailQueue.Exchange, MailQueue.RoutingKey);
-
-                        channel.BasicPublish(
-                            exchange: MailQueue.Exchange,
-                            routingKey: MailQueue.RoutingKey,
-                            mandatory: false,
-                            basicProperties: null,
-                            body: body);
-                    }
-                    catch (Exception ex)
-                    {
-                        throw new Exception("Exception while publishing a message", ex);
-                    }
-                    finally
-                    {
-                        channel.Close();
-                        connection.Close();
-                        channel.Dispose();
-                        connection.Dispose();
-                    }
-                }
             }
         }
     }
