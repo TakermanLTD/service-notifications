27e1baf37f247fcc2f241962add55bcb17fcdd82
Tanyo Ivanov <tivanov@takerman.net>
2024-10-20 17:32:21 +0300

    logging

--- Takerman.Notifications/Program.cs
+++ Takerman.Notifications/Program.cs
@@ -1,15 +1,13 @@
 using RabbitMq.Consumer.HostedServices;
 using RabbitMq.Consumer.Services;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.Slack;
-using Serilog.Sinks.Slack.Models;
+using Takerman.Logging;
 using Takerman.MailService.Consumer.HostedServices;
 using Takerman.MailService.Consumer.Middleware;
 using Takerman.MailService.Consumer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Host.AddTakermanLogging();
+builder.Logging.AddTakermanLogging();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,21 +15,6 @@ builder.Services.AddCommonService(builder.Configuration);
 builder.Services.AddTransient<IMailService, MailService>();
 builder.Services.AddSingleton<IConsumerService, ConsumerService>();
 builder.Services.AddHostedService<ConsumerHostedService>();
-
-Log.Logger = new LoggerConfiguration()
-    .MinimumLevel.Warning()
-    .ReadFrom.Configuration(builder.Configuration)
-    .WriteTo.Slack(new SlackSinkOptions
-    {
-        WebHookUrl = "https://hooks.slack.com/services/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",
-        CustomIcon = ":email:",
-        Period = TimeSpan.FromSeconds(10),
-        ShowDefaultAttachments = false,
-        ShowExceptionAttachments = true,
-        MinimumLogEventLevel = LogEventLevel.Error,
-        PropertyDenyList = ["Level", "SourceContext"]
-    })
-    .CreateLogger();
 
 var app = builder.Build();
 
